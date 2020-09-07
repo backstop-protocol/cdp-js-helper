0d35be9928d5ed1914f0127fcb9b61d728958475
yaron velner <yaron172@yahoo.com>
2020-09-07 14:01:13 +0300

    input verification functions

--- test/test.js
+++ test/test.js
@@ -115,7 +115,7 @@ contract('B Interface', function (accounts) {
     //console.log({userInfoAfter})
     assert(userInfoAfter.bCdpInfo.hasCdp,"user is expected to have a cdp")
     //assert.equal(userInfoAfter.bCdpInfo.ethDeposit.toString(10),web3.utils.toWei("4").toString(10),"user eth balance is expected to be 2")
-    assert.equal(userInfoAfter.bCdpInfo.daiDebt.toString(10),web3.utils.toWei("300").toString(10),"user eth balance is expected to be 2")
+    assert(closeEnough(web3.utils.fromWei(userInfoAfter.bCdpInfo.daiDebt.toString(10)),web3.utils.fromWei(web3.utils.toWei("300").toString(10))),"user eth balance is expected to be 2")
     assert(userInfoAfter.proxyInfo.hasProxy, "user is expected to have a proxy")
   })
 
@@ -230,16 +230,173 @@ contract('B Interface', function (accounts) {
     assert.equal(userInfoAfterAfter.bCdpInfo.ethDeposit.toString(10),web3.utils.toWei("14").toString(10),"user deposit is not as expected")
   })
 
+  it('calcNewBorrowLimitAndLiquidationPrice', async function () {
+    const user = accounts[2]
+
+    const depositVal = web3.utils.toWei("2") // 2 ETH
+    const txObject = B.firstDeposit(web3,user)
+    //console.log({txObject})
+    const gasConsumption = increaseABit(await txObject.estimateGas({value:depositVal,from:user}))
+    console.log({gasConsumption})
+    await txObject.send({gas:gasConsumption,value:depositVal,from:user})
+    await mineBlock()
+
+    let userInfo = await B.getUserInfo(web3,user)
+    const cdp = userInfo.bCdpInfo.cdp
+    console.log({cdp})
+
+    console.log("proxy",userInfo.proxyInfo.userProxy)
+
+    const withdrawalVal = web3.utils.toWei("150")
+    const txObject2 = B.generateDai(web3,userInfo.proxyInfo.userProxy,cdp,withdrawalVal)
+    const gasConsumption2 = increaseABit(await txObject2.estimateGas({from:user}))
+    console.log({gasConsumption2})
+    await txObject2.send({gas:gasConsumption2,from:user})
+    await mineBlock()
+
+
+    console.log("query user info again")
+    userInfo = await B.getUserInfo(web3,user)
+
+    const [maxDebt,newLiqPrice] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("0"),web3.utils.toWei("0"),web3)
+    assert.equal(web3.utils.fromWei(newLiqPrice),(150 * 1.5/2).toString())
+
+    const [maxDebt2,newLiqPrice2] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("4"),web3.utils.toWei("0"),web3)
+    assert(closeEnough(web3.utils.fromWei(newLiqPrice2),(150 * 1.5/6).toString()))
+
+    const [maxDebt3,newLiqPrice3] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("-1"),web3.utils.toWei("0"),web3)
+    assert.equal(web3.utils.fromWei(newLiqPrice3),(150 * 1.5).toString())
+
+    const [maxDebt4,newLiqPrice4] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("0"),web3.utils.toWei("150"),web3)
+    assert.equal(web3.utils.fromWei(newLiqPrice4),(300 * 1.5/2).toString())
+
+    const [maxDebt5,newLiqPrice5] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("0"),web3.utils.toWei("-50"),web3)
+    assert.equal(web3.utils.fromWei(newLiqPrice5),(100 * 1.5/2).toString())
+
+    const [maxDebt6,newLiqPrice6] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("1.5"),web3.utils.toWei("-50"),web3)
+    assert.equal(web3.utils.fromWei(newLiqPrice6),(100 * 1.5/3.5).toString())
+
+    userInfo.miscInfo.spotPrice = web3.utils.toWei("100.5")
+    userInfo.bCdpInfo.maxDaiDebt = web3.utils.toWei("134")
+
+    const [maxDebt7,newLiqPrice7] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("1"),web3.utils.toWei("-1"),web3)
+    assert.equal(web3.utils.fromWei(maxDebt7),(3*100.5 / 1.5).toString())
+    const [maxDebt8,newLiqPrice8] = B.calcNewBorrowLimitAndLiquidationPrice(userInfo,web3.utils.toWei("-1"),web3.utils.toWei("-1"),web3)
+    assert.equal(web3.utils.fromWei(maxDebt8),(1*100.5 / 1.5).toString())
+  })
+
+  it('input verification', async function () {
+    const user = accounts[3]
+
+    const depositVal = web3.utils.toWei("2") // 2 ETH
+    const txObject = B.firstDeposit(web3,user)
+    //console.log({txObject})
+    const gasConsumption = increaseABit(await txObject.estimateGas({value:depositVal,from:user}))
+    console.log({gasConsumption})
+    await txObject.send({gas:gasConsumption,value:depositVal,from:user})
+    await mineBlock()
+
+    let userInfo = await B.getUserInfo(web3,user)
+    const cdp = userInfo.bCdpInfo.cdp
+    console.log({cdp})
+
+    console.log("proxy",userInfo.proxyInfo.userProxy)
+
+    const withdrawalVal = web3.utils.toWei("150")
+    const txObject2 = B.generateDai(web3,userInfo.proxyInfo.userProxy,cdp,withdrawalVal)
+    const gasConsumption2 = increaseABit(await txObject2.estimateGas({from:user}))
+    console.log({gasConsumption2})
+    await txObject2.send({gas:gasConsumption2,from:user})
+    await mineBlock()
+
+
+    console.log("query user info again")
+    userInfo = await B.getUserInfo(web3,user)
+
+    // verify deposit
+    const [succ1,msg1] = B.verifyDepositInput(userInfo, web3.utils.toWei("-1"),web3)
+    assert(! succ1, "verifyDepositInput should fail")
+    assert.equal(msg1,"Deposit amount must be positive")
+
+    const balancePlusOne = web3.utils.toBN(userInfo.userWalletInfo.ethBalance).add(web3.utils.toBN(1e10))
+    const [succ2,msg2] = B.verifyDepositInput(userInfo, balancePlusOne.toString(10),web3)
+    assert(! succ2, "verifyDepositInput should fail")
+    assert.equal(msg2,"Amount exceeds wallet balance")
+
+    const balanceMinusOne = web3.utils.toBN(userInfo.userWalletInfo.ethBalance).sub(web3.utils.toBN(1e10))
+    const [succ3,msg3] = B.verifyDepositInput(userInfo, balanceMinusOne.toString(10),web3)
+    assert(succ3, "verifyDepositInput should pass")
+    assert.equal(msg3,"")
+
+    // verify withdraw ETH
+    const [succ4,msg4] = B.verifyWithdrawInput(userInfo,web3.utils.toWei("-1"),web3)
+    assert(! succ4, "verifyWithdrawInput should failed")
+    assert.equal(msg4,"Withdraw amount must be positive")
+
+    const [succ5,msg5] = B.verifyWithdrawInput(userInfo,web3.utils.toWei("2.001"),web3)
+    assert(! succ5, "verifyWithdrawInput should failed")
+    assert.equal(msg5,"Amount exceeds CDP deposit")
+
+    const [succ6,msg6] = B.verifyWithdrawInput(userInfo,web3.utils.toWei("1.9"),web3)
+    assert(! succ6, "verifyWithdrawInput should failed")
+    assert.equal(msg6,"Amount exceeds allowed withdrawal")
+
+    const [succ7,msg7] = B.verifyWithdrawInput(userInfo,web3.utils.toWei("0.1"),web3)
+    assert(succ7, "verifyWithdrawInput should pass")
+
+    // verify borrow dai
+    const [succ8,msg8] = B.verifyBorrowInput(userInfo, web3.utils.toWei("-1"),web3)
+    assert(! succ8, "verifyBorrowInput should fail")
+    assert.equal(msg8,"Borrow amount must be positive")
+
+    const [succ9,msg9] = B.verifyBorrowInput(userInfo, web3.utils.toWei("1000"),web3)
+    assert(! succ9, "verifyBorrowInput should fail")
+    assert.equal(msg9,"Amount exceeds allowed borrowed")
+
+    const [succ10,msg10] = B.verifyBorrowInput(userInfo, web3.utils.toWei("100"),web3)
+    assert(succ10, "verifyBorrowInput should pass")
+
+    // verify repay dai
+    const [succ11,msg11] = B.verifyRepayInput(userInfo,web3.utils.toWei("-1"),web3)
+    assert(! succ11, "verifyRepayInput should failed")
+    assert.equal(msg11,"Repay amount must be positive")
+
+    userInfo.userWalletInfo.daiAllowance = web3.utils.toWei("190")
+    const [succ12,msg12] = B.verifyRepayInput(userInfo,web3.utils.toWei("150.0001"),web3)
+    assert(! succ12, "verifyRepayInput should failed")
+    assert.equal(msg12,"Amount exceeds dai balance")
+
+    userInfo.userWalletInfo.daiAllowance = web3.utils.toWei("100")
+    const [succ13,msg13] = B.verifyRepayInput(userInfo,web3.utils.toWei("100.0001"),web3)
+    assert(! succ13, "verifyRepayInput should failed")
+    assert.equal(msg13,"Must unlock DAI")
+
+    userInfo.userWalletInfo.daiAllowance = web3.utils.toWei("200")
+    userInfo.userWalletInfo.daiBalance = web3.utils.toWei("200")
+    const [succ14,msg14] = B.verifyRepayInput(userInfo,web3.utils.toWei("150.0001"),web3)
+    assert(! succ14, "verifyRepayInput should failed")
+    assert.equal(msg14,"Amount exceeds dai debt")
+
+    const [succ15,msg15] = B.verifyRepayInput(userInfo,web3.utils.toWei("100.0031"),web3)
+    assert(succ15, "verifyRepayInput should pass")
+  })
 })
 
 function increaseABit(number) {
-  return parseInt(1.1 * Number(number))
+  return parseInt(1.2 * Number(number))
 }
 
 function sleep (ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms)
   })
+}
+
+function closeEnough(a,b) {
+  if(Number(a) > Number(b)) return closeEnough(b,a)
+  if(Number(a)/Number(b) < 0.9999) return false
+
+  return true;
 }
 
 async function sendEth (from, to, amountInWei) {
